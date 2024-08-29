d4449fd16a44e5e9678df6cd1292844007952f73
Alfie Wilkinson <alfie@whyougetproductions.com>
2024-08-29 17:37:51 +0100

    Update script.js

--- red/29182374/script.js
+++ red/29182374/script.js
@@ -1,4 +1,2 @@
-console.warn("PLEASE NOTE THIS URL IS NOT OPERATED BY HTTPS://ALFZ.UK BUT IS HOWEVER OWNED BY A SUBISIDARY, PLEASE PROCEED");
-
 window.location.href = "https://whyougetproductions.com"
 print("Succesfully redirected to specified URL")
